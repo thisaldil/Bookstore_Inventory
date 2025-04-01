5ca64880b074b815955d14c8c32d4f0b029c048d
thisaldil <tdimith10@gmail.com>
2025-04-01 10:18:29 +0530

    Initial commit

---
+++ Bookstore_Inventory/src/main/java/com/example/Bookstore_Inventory/BookstoreInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bookstore_Inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreInventoryApplication.class, args);
+	}
+
+}
---
+++ Bookstore_Inventory/src/test/java/com/example/Bookstore_Inventory/BookstoreInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bookstore_Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
